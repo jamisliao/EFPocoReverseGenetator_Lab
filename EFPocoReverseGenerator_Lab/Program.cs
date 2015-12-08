287288a329517a01c3d513b996c556e47ec2294f
jamisliao <jamisliao@nine-yi.com>
2015-12-08 15:04:04 +0800

    First commit

---
+++ EFPocoReverseGenerator_Lab/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFPocoReverseGenerator_Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var product = new Product
+            {
+                Id = Guid.NewGuid(),
+                Name = "Test",
+                Price = 100
+            };
+
+            using (var context = new PocoReverseDBContext())
+            {
+                context.Product.Add(product);
+                context.SaveChanges();
+            }
+        }
+    }
+}
